598bf7c44acc0c42ded380dc6a0efe4d4a407c19
iyiakhkun <sundayiyigulu404@gmail.com>
2022-12-22 07:12:18 -0800

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,6 +5,7 @@
  * @s2: against this other string 2
  *
  * Return: 0 if s1 and s2 are equal
+ *
  *        negative integer if the stopping character
  *                in @s1 was less than the stopping
  *                character in @s2
@@ -12,6 +13,7 @@
  *                in @s1 was greater than the stopping
  *                character in @s2
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0, r;
@@ -20,7 +22,7 @@ int _strcmp(char *s1, char *s2)
 	 * iterate through src and
 	 * compare it with dest
 	 */
-	while (s1[i] != '\0' s2[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		/**
 		 * if they differ by a single character
@@ -29,7 +31,7 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] != s2[i])
 		{
 			/*
-			 * return the difference between
+			 * return teh difference between
 			 * the two characters
 			 */
 			r = s1[i] - s2[i];
